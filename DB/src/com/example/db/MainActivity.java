e72ec9e2d8fee847e26ae72a7cbc91592f7f6023
PeterOnyango <okinyi.peter.onyango@gmail.com>
2016-12-05 04:22:25 -0800

    first commit

---
+++ DB/src/com/example/db/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.db;
+
+import org.apache.http.Header;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import android.view.View.OnClickListener;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	
+	EditText fname,lname,mname;
+	Button save;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		fname=(EditText)findViewById(R.id.fname);
+		mname=(EditText)findViewById(R.id.mname);
+		lname=(EditText)findViewById(R.id.lname);
+		save=(Button)findViewById(R.id.save);
+		
+		save.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0)
+			{
+				// TODO Auto-generated method stub
+				
+				String a=fname.getText().toString();
+				String b=mname.getText().toString();
+				String c=lname.getText().toString();
+				
+			   //Object that will make request to the server
+				AsyncHttpClient client=new AsyncHttpClient();
+				
+				//Object to request parameters that will be sent to the server
+				RequestParams params=new RequestParams();
+				params.put("fname", a);
+				params.put("mname", b); 
+				params.put("lname", c);
+				
+				client.post("http://10.0.2.2",params, new AsyncHttpResponseHandler() {
+					
+					@Override
+					public void onSuccess(int arg0, Header[] arg1, byte[] response) 
+					{
+						// TODO Auto-generated method stub
+						String serverresponse=new String(response);
+						
+						if(serverresponse.equals("3")){
+							Toast.makeText(getApplicationContext(),"All Details Required",Toast.LENGTH_LONG).show();
+						}else if(serverresponse.equals("2")){
+							Toast.makeText(getApplicationContext(),"Records Already in The Database",Toast.LENGTH_LONG).show();
+						}else if(serverresponse.equals("4"))
+						
+						{Toast.makeText(getApplicationContext(),"Record Saved Successfully",Toast.LENGTH_LONG).show();}
+						
+						else if(serverresponse.equals("5"))
+							
+						{Toast.makeText(getApplicationContext(),"Problem Communicating with server",Toast.LENGTH_LONG).show();}
+						
+					}
+					
+					@Override
+					public void onFailure(int arg0, Header[] arg1, byte[] arg2, Throwable arg3) {
+						// TODO Auto-generated method stub
+						Toast.makeText(getApplicationContext(),"Unable to access server",Toast.LENGTH_LONG).show();
+						
+					}
+				});
+				
+			}
+		});
+		
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
